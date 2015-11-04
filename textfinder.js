54799c1a1484b44a08358ca005ce7c3953000cb2
rudolph.koegelenberg <rudolph.koegelenberg@hellocomputer.com>
2015-11-04 20:30:17 +0200

    initial commit

---
+++ textfinder.js
@@ -0,0 +1,26 @@
+// Constructor
+function TextFinder(wordList) {
+  this.wordlist = wordList;
+  this.searchstring = new RegExp(wordList.join(" |").replace(/[^\w\s^|]/gi, ''), 'i');
+  this.globalsearchstring = new RegExp(wordList.join(" |").replace(/[^\w\s^|]/gi, ''), 'gi');
+
+}
+// class methods
+TextFinder.prototype.contains = function(content) {
+	return this.searchstring.test(content);
+};
+
+TextFinder.prototype.matches = function(content) {
+    return content.match(this.globalsearchstring);
+};
+
+TextFinder.prototype.removeWords = function(content) {
+    return content.replace(this.globalsearchstring, '');
+};
+
+TextFinder.prototype.replaceWords = function(content, replacestr) {
+    return content.replace(this.globalsearchstring, replacestr);
+};
+
+
+module.exports = TextFinder;
